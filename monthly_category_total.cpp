643ee40f0832313f10c41534b7a3140ae8019ab7
ZZMHunter <zhao.zhang50@myhunter.cuny.edu>
2025-09-18 17:20:44 -0400

    .

--- monthly_category_total.cpp
+++ monthly_category_total.cpp
@@ -78,6 +78,7 @@ int main(){
             monthArray[month(day)-1] += std::stod(cost);
         }
     }
+    std::cout << std::endl;
     std::cout << "Month, " << categorySeperator(categoryArray[choice])<< ", " << "TOTAL" << std::endl;
     for (int i = 0; i < 12; i++){
         std::cout << monthArr[i] << ", " <<  monthArray[i] << std::endl;
